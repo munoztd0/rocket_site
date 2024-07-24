e89701e1245df6ed9970d3397464aebe063e6718
munoztd0 <david.munoztord@mailbox.org>
2024-07-24 11:23:57 +0900

    done

--- js/myfunc.js
+++ js/myfunc.js
@@ -104,17 +104,31 @@ function getPoidsPrice() {
 }
 
 function doSomething(event) {
+    // Prevent the default form submission
     event.preventDefault();
     
-    if (!pickup1.lastSelected || !delivery.lastSelected) {
-        alert("Please select both pickup and delivery locations.");
-        return false;
+    console.log("Function doSomething called");
+
+    try {
+        var typePrice = getTypePrice();
+        var distPrice = getDistPrice();
+        var urgPrice = getUrgPrice();
+        var poidsPrice = getPoidsPrice();
+
+        console.log("Prices calculated:", {typePrice, distPrice, urgPrice, poidsPrice});
+
+        var totPrice = ((typePrice + distPrice) * urgPrice) + poidsPrice;
+        
+        console.log("Total price calculated:", totPrice);
+
+        var divobj = document.getElementById('totalPrice');
+        divobj.style.display = 'block';
+        divobj.innerHTML = totPrice + " CHF";
+    } catch (error) {
+        console.error("Error in doSomething:", error);
+        alert("An error occurred while calculating the price. Please try again.");
     }
-    
-    var totPrice = ((getTypePrice() + getDistPrice()) * getUrgPrice()) + getPoidsPrice();
-    var divobj = document.getElementById('totalPrice');
-    divobj.style.display = 'block';
-    divobj.innerHTML = totPrice + " CHF";
+
     return false;
 }
 
